b68603b26b50b19670358707488bc8fe4252fabf
nikololiahim <olomishcak@outlook.com>
2024-10-11 18:03:49 +0300

    add nix-tree to a devshell

--- flake.nix
+++ flake.nix
@@ -136,6 +136,7 @@
           buildInputs = with pkgs; [
             # Tools for Lua and Nix development, useful for editing files in this repo
             lua-language-server
+            nix-tree
             inputs.nixd.packages.${system}.default
           ];
           shellHook = ''
