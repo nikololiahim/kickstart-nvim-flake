48cbf91ccbd3d45a7f0705eca4f839878ede415f
nikololiahim <olomishcak@outlook.com>
2024-11-30 00:56:46 +0300

    close help with escape

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,3 +12,11 @@ vim.opt.expandtab = true
 vim.opt.showmode = false
 vim.opt.splitright = true
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'help',
+  callback = function(event)
+    vim.keymap.set('n', '<esc>', function()
+      vim.cmd.helpclose()
+    end, { buffer = event.buf })
+  end,
+})
