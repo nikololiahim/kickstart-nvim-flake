1f5e4f4f9c88834b4cad8c414fe2347c05cba17d
nikololiahim <olomishcak@outlook.com>
2025-01-14 00:13:47 +0500

    disable flash.nvim for f, F, t, T

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,10 +1,14 @@
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
-  ---@type Flash.Config
   opts = {
     jump = {
       autojump = true,
+    },
+    modes = {
+      char = {
+        keys = { ';', ',' },
+      },
     },
   },
   -- stylua: ignore
